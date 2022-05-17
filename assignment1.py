1988dad52e8b12947481e2726752faef79095659
collins1900 <collinsmiriti@gmial.com>
2022-05-17 20:09:54 +0300

    lessons

--- assignment1.py
+++ assignment1.py
@@ -4,3 +4,19 @@ radius = input("Enter the radius of circle")
 PI = 3.142 
 fom = PI * enterRadiusOfCircle * enterRadiusOfCircle
 print("Area of circle" + str(fom))
+
+#volume of cylinder
+enterRadiusOfCircle = 7
+radius = input("Enter the radius of circle")
+PI = 3.142
+enterHeightOfCylinder = 10
+height = input("Enter height of cylinder")
+fom = PI * enterRadiusOfCircle * enterRadiusOfCircle * enterHeightOfCylinder
+print("volume of cylinder" + str(fom))
+
+#volume of cube
+enterLengthOfCube = 10
+lengh = input("Enter length of cube")
+fom = enterLengthOfCube * enterLengthOfCube *enterLengthOfCube
+print("volume of cube" + str(fom))
+
