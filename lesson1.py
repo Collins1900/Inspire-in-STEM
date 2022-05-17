5b147e19db234ce00727d749b6511fe1a76555de
collins1900 <collinsmiriti@gmial.com>
2022-05-17 11:44:05 +0300

    lesson2 about integers

---
+++ lesson1.py
@@ -0,0 +1,17 @@
+#python basic
+
+#This is a comment
+
+#Adding two number
+
+#This is a
+#Multi / many lines
+#Comments
+
+print("My name is Collins Murimi")
+print("I am from Nairobi")
+
+print("I attended Mang'u High School ")
+print("My favourite country is Kenya")
+
+print("I like swimming")
---
+++ lesson2.py
@@ -0,0 +1,24 @@
+#my age
+
+Age = 18
+
+no_of_siblings = 3
+noOfSiblings = 3
+
+dateOfBirth = 28
+yearOfBirth = 2003
+
+#strings
+my_first_name = "collins"
+my_second_name = "murimi"
+
+favouriteFood = "snails"
+schoolattended = ("Mang'u High School")
+
+print("I am " + my_first_name)
+print("I am " + my_second_name)
+
+print("I am " + str (Age) + " years old")
+
+print("I like " + favouriteFood)
+print("I attended " + schoolattended)
