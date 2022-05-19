9e43e57951dddb117455df6b3c59b676268c1936
collins1900 <collinsmiriti@gmial.com>
2022-05-19 11:17:10 +0300

    lesson on lists and loops

---
+++ lesson6.py
@@ -0,0 +1,32 @@
+# Learning about lists
+motocycles_owner = "Mojo Jojo"
+plate_number = ['H1234','Y1234','S1234']
+motocycles = ['honda', 'yamaha', 'suzuki']
+#accessing list using index
+#print(motocycles)
+#motocycles[1] = "Bugatti"#changing element in a list
+#print("I love" + str(motocycles[1]))
+#adding element in a list
+#motocycles.append('Bugatti','Yamaha')#append Item into a list
+#print(motocycles[0],[1],[2])
+#print(plate_number[0])
+#print(motocycles[0],plate_number[0])
+print(motocycles[0],plate_number[0])
+print(motocycles[1],plate_number[1])
+print(motocycles[2],plate_number[2])
+#deleting an item from a list
+#del motocycles[2] #deleting an item from a list
+#print(motocycles)
+
+popped_motocycles = motocycles.pop()
+#print(motocycles) 
+#task print a statement
+#My name is Mojo Jojo and I own a motocycle plate number
+print(f"My name is {motocycles_owner} and I own a {motocycles[0]} plate number{plate_number[0]}")
+
+#removing an item from a list
+#motocycles.remove('honda')
+#print(motocycles)
+
+
+
---
+++ loops.py
@@ -0,0 +1,15 @@
+school= ['Joy','Hope','Mercy','Happy']
+pupil= ['Peace','Patience','Amani','Charity']
+#Hard ware
+#print(school[0],pupil[0])
+#print(school[1],pupil[1])
+#print(school[2],pupil[2])
+#print(school[3],pupil[3])
+
+#print(f"I am (pupil[0]) and I school at (school[0])")
+#print(f"I am (pupil[1]) and I school at (school[1])")
+#print(f"I am (pupil[2] and I school at school[2]")
+#print(f"I am  (pupil[3] and I school at school[3]")
+for pupil in pupil:
+    print(f'Hello I am {pupil}')
+    
