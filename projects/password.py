cb71dd21ed9ab7f634bc40d4eb90bb245de17ee6
collins1900 <collinsmiriti@gmial.com>
2022-06-03 11:57:33 +0300

    lesson on projects(password),functions

---
+++ projects/password.py
@@ -0,0 +1,12 @@
+import random
+
+print("Welcome to our password generator")
+character = 'student@001'
+num_password = int(input("how many passwords do you want to generate?"))
+len_password = int(input("what is the lengh of your password"))
+print("\n Here are your passwords")
+for password in range (num_password):
+    password = ''
+    for c in range (len_password):
+        password+= random.choice(character)
+    print(password)
